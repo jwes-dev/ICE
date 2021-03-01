b6a1f1d1b85e441be01a3f755945b2b8b6a9cbea
John Wesley M <johnwesley.m@outlook.com>
2021-03-02 00:50:44 +0530

    Create ice.js

---
+++ ice.js
@@ -0,0 +1,49 @@
+/**
+ * Author: John Wesley M
+ * Interface Component Element
+ */
+
+export class ICEScope {
+	/**
+	 * Scope class. Creates a node with only ice
+	 * @param {HTMLElement} ScopeElement 
+	 */
+	constructor(ScopeElement) {
+		let elements = ScopeElement.querySelectorAll("[ice-name]");
+		let name = '';
+		for (let ice of elements) {
+			name = ice.getAttribute("ice-name");
+			if(name.endsWith("[]")) {
+				name = name.replace("[]", "");
+				if(this[name] !== undefined) {
+					this[name].push(ice);
+				} else {
+					this[name] = [ice];
+				}
+			} else {
+				this[name] = ice;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param {string} AppName 
+	 */
+	static Init(AppName) {
+
+	}
+}
+
+export class Controller {
+	constructor() {
+		this.Scope = null;
+	}
+	Init() {
+		this.App = document.querySelector("[ice-app='" + this.constructor.name + "']");
+		if (this.App === null) {
+			throw "App " + AppName + " not found";
+		}
+		this.Scope = new ICEScope(this.App);
+	}
+}
